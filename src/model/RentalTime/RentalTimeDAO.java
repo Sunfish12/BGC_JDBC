897be3779376ff585a463eafb721b2a8959a3826
Sunfisher12 <qaz22881757@gmail.com>
2014-12-24 18:15:58 +0800

    final JDBC
    Signed-off-by: Sunfisher12 <qaz22881757@gmail.com>

--- src/model/RentalTime/RentalTimeDAO.java
+++ src/model/RentalTime/RentalTimeDAO.java
@@ -2,17 +2,11 @@ package model.RentalTime;
 
 import java.io.InputStream;
 import java.util.List;
-
-public interface RentalTimeDAO {
+public interface RentalTimeDAO 
+{
 	public abstract RentalTimeBean select(Integer storeId);
-
 	public abstract List<RentalTimeBean> select();
-
-	public abstract RentalTimeBean insert(RentalTimeBean rtbean,
-			InputStream is, long size);
-
-	public abstract RentalTimeBean update(RentalTimeBean rtbean,
-			InputStream is, long size);
-
+	public abstract RentalTimeBean insert(RentalTimeBean rtbean);
+	public abstract RentalTimeBean update(RentalTimeBean rtbean);
 	public abstract boolean delete(Integer storeId);
 }
